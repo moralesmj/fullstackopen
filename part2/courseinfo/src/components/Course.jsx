001fa89c510058f87c6265cf9148984fbbd66d0c
moralesmj <moralesmjdev@gmail.com>
2026-01-15 14:55:05 -0600

    course information 2.4-2.5

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,33 +1,37 @@
-const Header = (props) => <h1>{props.course}</h1>
+const Header = ({ course }) => <h1>{course.name}</h1>
 
-const Content = (props) => (
-    <div>
-        <Part part={props.parts[0]} />
-        <Part part={props.parts[1]} />
-        <Part part={props.parts[2]} />
-        <Part part={props.parts[3]} />
-    </div>
-)
-
-const Part = (props) => (
-    <p>
-        {props.part.name} {props.part.exercises}
-    </p>
-)
-
-const Total = (props) => <b>total of {props.total} exercises</b>
-
-const Course = ({ course }) => {
-    const total = course.parts.reduce(
-        (counter, value) => counter + value.exercises, 0
+const Content = ({ parts }) => (
+    parts.map(part =>
+        <Part
+            key={part.id}
+            name={part.name}
+            exercises={part.exercises}
+        />
     )
+)
 
+const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
+
+const Total = ({ total }) => <b>total of {total} exercises</b>
+
+const Course = ({ courses }) => {
+    
     return (
-        <div>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total total={total}/>
-        </div>
+        <>
+            {courses.map(course => {
+                const total = course.parts.reduce(
+                    (counter, value) => counter + value.exercises, 0
+                )
+
+                return (
+                    <div key={course.id}>
+                        <Header course={course} />
+                        <Content parts={course.parts} />
+                        <Total total={total}/>
+                    </div>
+                )
+            })}
+        </>
     )
 }
 
