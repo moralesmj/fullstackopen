811c2f878e08f71d9ad9974224c44253efd02334
moralesmj <moralesmjdev@gmail.com>
2026-02-20 19:20:05 -0600

    uibuild

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -37,6 +37,7 @@ let persons = [
     }
 ]
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
