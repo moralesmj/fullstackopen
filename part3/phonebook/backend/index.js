50ba83afdacc35575adaa97d1a88733e498aa26b
moralesmj <moralesmjdev@gmail.com>
2026-02-24 11:56:26 -0600

    the phonebook 3.13

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
 
 const app = express()
 
@@ -41,7 +43,9 @@ app.use(express.static('dist'))
 app.use(express.json())
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -109,7 +113,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
